3533ce1bd44daf13f990f09adabc80dce1cab700
kendeclerck <k.declerck@ccvlab.eu>
2023-07-29 13:51:26 +0200

    Refactor if clauses to less use cases by grouping and removing

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -17,50 +17,41 @@ class GildedRose {
         item.quality = Math.min(item.quality + amount, 50);
     }
 
+    private static boolean sellByDateHasPassed(Item item) {
+        return item.sellIn < 0;
+    }
+
     public void updateQuality() {
         Arrays.stream(items).forEach(item -> {
             if (item.name.equals(SULFURAS)) {
                 return;
             }
-            if (!item.name.equals(AGED_BRIE)
-                && !item.name.equals(BACKSTAGE_PASSES)
-                && item.quality > 0) {
-                item.quality = item.quality - 1;
-            } else {
-                if (item.name.equals(BACKSTAGE_PASSES)) {
-                    if (item.sellIn > 11) {
-                        addQuality(item, 1);
-                    } else if (item.sellIn < 11 && item.sellIn > 6) {
-                        addQuality(item, 2);
-                    } else if (item.sellIn < 6 && item.sellIn >= 0) {
-                        addQuality(item, 3);
-                    } else if (item.sellIn < 0) {
-                        item.quality = 0;
-                    }
+            item.sellIn = item.sellIn - 1;
+
+            if (item.name.equals(AGED_BRIE)) {
+                if (sellByDateHasPassed(item)) {
+                    addQuality(item, 2);
                 } else {
                     addQuality(item, 1);
                 }
-            }
-
-            if (!item.name.equals(SULFURAS)) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.quality > 0 && (!item.name.equals(SULFURAS))) {
-                            item.quality = item.quality - 1;
-                        }
-                    } else {
-                        item.quality = 0;
-                    }
+            } else if (item.name.equals(BACKSTAGE_PASSES)) {
+                if (item.sellIn > 10) {
+                    addQuality(item, 1);
+                } else if (item.sellIn < 10 && item.sellIn > 5) {
+                    addQuality(item, 2);
+                } else if (item.sellIn < 5 && item.sellIn >= 0) {
+                    addQuality(item, 3);
+                } else if (sellByDateHasPassed(item)) {
+                    item.quality = 0;
+                }
+            } else if (item.quality > 0) {
+                if (sellByDateHasPassed(item)) {
+                    item.quality = item.quality - 2;
                 } else {
-                    if (item.quality < 50) {
-                        addQuality(item, 1);
-                    }
+                    item.quality = item.quality - 1;
                 }
             }
+
         });
     }
 }
